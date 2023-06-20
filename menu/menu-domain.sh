f6df05e270039caa4d22edc5e86df50074d475f0
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-21 01:23:12 +0800

    Update menu-domain.sh

--- menu/menu-domain.sh
+++ menu/menu-domain.sh
@@ -20,7 +20,7 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-host ;;
-2) clear ; certv2ray ;;
+2) clear ; cert ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "Anda salah tekan" ; sleep 1 ; menu-domain ;;
