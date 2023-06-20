9d67feca9147f43907252344106d7bc6c9d88d8d
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-21 07:57:25 +0800

    @Baung

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,76 @@
 #!/bin/bash
-# My Telegram : https://t.me/@Baung2012
-# ==========================================
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/kuhing/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m"
+export COLOR1="$(cat /etc/kuhing/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/kuhing/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\033[0;33m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -29,20 +99,27 @@ $email
 EOF
 fi
 clear
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
+echo "\033[0;34m Mohon Menunggu , Proses Backup sedang berlangsung !!! \033[0m"
 rm -rf /root/backup
 mkdir /root/backup
-cp /etc/passwd backup/
-cp /etc/group backup/
-cp /etc/shadow backup/
-cp /etc/gshadow backup/
-cp /etc/crontab backup/
-cp -r /var/lib backup/
-cp -r /etc/xray backup/xray
-cp -r /etc/slowdns backup/slowdns
-cp -r /etc/per/id backup/id
-cp -r /etc/per/token backup/token
-cp -r /var/www/html/ backup/html
+cp -r /root/.acme.sh /root/backup/ > /dev/null 2>&1
+cp /etc/passwd backup/ > /dev/null 2>&1
+cp /etc/group backup/ > /dev/null 2>&1
+cp /etc/shadow backup/ > /dev/null 2>&1
+cp /etc/gshadow backup/ > /dev/null 2>&1
+cp /etc/crontab backup/ > /dev/null 2>&1
+cp -r /var/lib backup/ > /dev/null 2>&1
+cp -r /etc/xray/config.json backup/ > /dev/null 2>&1
+cp -r /etc/xray backup/xray > /dev/null 2>&1
+cp -r /etc/per/id backup/id > /dev/null 2>&1
+cp -r /etc/per/token backup/token > /dev/null 2>&1
+cp -r /etc/slowdns backup/slowdns > /dev/null 2>&1
+cp -r /etc/slowdns/server.pub backup/server.pub > /dev/null 2>&1
+cp -r /etc/slowdns/server.key backup/server.key > /dev/null 2>&1
+cp -r /etc/nginx/conf.d /root/backup/conf.d/ > /dev/null 2>&1
+cp -r /home/vps/public_html /root/backup/public_html > /dev/null 2>&1
+cp -r /etc/cron.d /root/backup/cron.d > /dev/null 2>&1
+cp -r /etc/crontab /root/backup/crontab > /dev/null 2>&1
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
