4c1af5eee1f55f35fe560be66f9044ee0c48caeb
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-23 10:34:43 +0800

    @Baung

--- ssh/notif.sh
+++ ssh/notif.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+clear
 TIMES="10"
 CHATID="847645599"
 KEY="6208240566:AAFINY02Hij6uwZo1rbgSLoyb4qBeT4p7RA"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
-curl -s ipinfo.io/city?token=75082b4831f909 >> /etc/xray/city
-curl -s ipinfo.io/org?token=75082b4831f909  | cut -d " " -f 2-10 >> /etc/xray/isp
+curl -s ipinfo.io/city?token=75082b4831f909 >> /etc/lokasi/city
+curl -s ipinfo.io/org?token=75082b4831f909  | cut -d " " -f 2-10 >> /etc/lokasi/isp
 IP=$(echo $SSH_CLIENT | awk '{print $1}')
 TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
 curl http://ipinfo.io/$IP -s -o $TMPFILE
@@ -14,8 +15,8 @@ LocalVersion=$(cat /root/versi)
 IPVPS=$(curl -s ipinfo.io/ip )
 ISPVPS=$( curl -s ipinfo.io/org )
 Exp=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $3}')
-ISP=$(cat /etc/xray/isp)
-CITY=$(cat /etc/xray/city)
+ISP=$(cat /etc/lokasi/isp)
+CITY=$(cat /etc/lokasi/city)
 domain=$(cat /etc/xray/domain) 
 ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
 tmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
