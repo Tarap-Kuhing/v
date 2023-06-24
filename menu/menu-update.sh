c091528faa5c6ad5c82b5d2533c8af06072e1172
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-24 15:41:42 +0800

    @Baung

--- menu/menu-update.sh
+++ menu/menu-update.sh
@@ -210,9 +210,9 @@ wget -O cf "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/cf.sh"
 wget -O menu-bckp "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/menu/menu-bckp.sh"
 wget -O add-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/add-ssws.sh"
 wget -O add-v2ray "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/xray/add-v2ray.sh"
+wget -O wireguard "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/wireguard/add-wireguard.sh"
 wget -O add-ns "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/add-ns.sh"
 wget -O issue "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/ssh/issue.net"
-wget -O wireguard "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/wireguard/add-wireguard.sh"
 
 chmod +x menu-update
 chmod +x update
