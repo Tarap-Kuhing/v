1ca88fe7a75168e284f79a2df84562dcc835fb75
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-30 20:51:10 +0800

    @Baung

--- gotop.sh
+++ gotop.sh
@@ -9,10 +9,10 @@ chmod +x /usr/sbin/speedtest
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
 curl -sL "$gotop_link" -o /tmp/gotop.deb
-dpkg -i /tmp/gotop.deb >/dev/null 2>&1
+dpkg -i /tmp/gotop.deb
 
 # > pasang glow
 glow_base="$(curl -s https://api.github.com/repos/charmbracelet/glow/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 glow_latest="https://github.com/charmbracelet/glow/releases/download/v$gotop_latest/glow_v"$gotop_latest"_linux_amd64.deb"
-curl -sL "$glow_latest" -o /tmp/glow.deb
-dpkg -i /tmp/glow.deb >/dev/null 2>&1
+curl -sL "$glow_latest"
+chmod +x /usr/sbin/gotop
