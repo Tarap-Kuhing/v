2a16a8cb1c98e503ef1faf9cd5c59cdeee50c007
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-07-02 18:24:44 +0800

    @Baung

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -48,10 +48,51 @@ wget -O backup "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/bac
 wget -O restore "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/restore.sh"
 wget -O strt "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/strt.sh"
 wget -O limitspeed "https://raw.githubusercontent.com/Tarap-Kuhing/v/main/backup/limitspeed.sh"
+wget -O xp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/ssh/xp.sh"
+
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
 chmod +x strt
 chmod +x limitspeed
+chmod +x xp
+cd
+if [ ! -f "/etc/cron.d/cleaner" ]; then
+cat> /etc/cron.d/cleaner << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/2 * * * * root /usr/bin/cleaner
+END
+fi
+
+if [ ! -f "/etc/cron.d/re_otm" ]; then
+cat> /etc/cron.d/re_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
+fi
+
+if [ ! -f "/etc/cron.d/xp_otm" ]; then
+cat> /etc/cron.d/xp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+fi
+cat > /home/re_otm <<-END
+7
+END
+
+if [ ! -f "/etc/cron.d/bckp_otm" ]; then
+cat> /etc/cron.d/bckp_otm << END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 5 * * * root /usr/bin/bottelegram
+END
+fi
+
+service cron restart > /dev/null 2>&1
 rm -f /root/set-br.sh
+
 echo ""
