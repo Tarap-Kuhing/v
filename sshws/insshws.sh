20db4eb2d83f47e1fcc588704007fc44aabb338d
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-07-06 22:18:58 +0800

    @Baung

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -1,98 +1,42 @@
 #!/bin/bash
-# Mod by Tarap Kuhing Tunneling
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
+#installer Websocker tunneling 
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/dropbear-ws
+cd
+
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-ovpn
+
+#izin permision
 chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+chmod +x /usr/local/bin/ws-ovpn
 
-# Installing Service
-cat > /etc/systemd/system/ws-dropbear.service << END
-[Unit]
-Description=Python Proxy Mod By T A R A P - K U H I N G
-Documentation=https://t.me/@Baung2012
-After=network.target nss-lookup.target
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear
-Restart=on-failure
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
-[Install]
-WantedBy=multi-user.target
-END
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
+##System Websocket-OpenVPN Python
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+
+#restart service
+#
 systemctl daemon-reload
+#Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
 systemctl restart ws-dropbear.service
 
-wget -O /usr/local/bin/ws-openvpn https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-openvpn
-chmod +x /usr/local/bin/ws-openvpn
-
-# Installing Service
-cat > /etc/systemd/system/ws-openvpn.service << END
-[Unit]
-Description=Python Proxy Mod By T A R A P - K U H I N G
-Documentation=https://t.me/@Baung2012
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-openvpn
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable ws-openvpn.service
-systemctl restart ws-openvpn.service
-
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ws-stunnel
-chmod +x /usr/local/bin/ws-stunnel
-
-# Installing Service
-cat > /etc/systemd/system/ws-stunnel.service << END
-[Unit]
-Description=Python Proxy Mod By T A R A P - K U H I N G
-Documentation=https://t.me/@Baung2012
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
+#Enable & Start & Restart ws-openssh service
 systemctl enable ws-stunnel.service
 systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
+
+#Enable & Start ws-ovpn service
+systemctl enable ws-ovpn.service
+systemctl start ws-ovpn.service
+systemctl restart ws-ovpn.service
