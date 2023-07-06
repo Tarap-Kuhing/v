d7a513e3114f3f8e69c01cc03e2e3c26a4a8c791
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-07-06 22:44:43 +0800

    @Baung

--- OPENVPN/ohp.sh
+++ OPENVPN/ohp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Ohp Script
-# Mod By none
+# Mod By TARAP KUHING TUNNELING
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -15,9 +15,10 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
-wget https://raw.githubusercontent.com/Tarap-Kuhing/v/main/sshws/ohpserver
+https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/sshws/ohpserver-linux32.zip
+unzip ohpserver-linux32.zip
 chmod +x ohpserver
-cp ohpserver /usr/local/ohpserver
+cp ohpserver /usr/local/bin/ohpserver
 /bin/rm -rf ohpserver*
 
 # Installing Service
@@ -25,7 +26,7 @@ cp ohpserver /usr/local/ohpserver
 cat > /etc/systemd/system/ssh-ohp.service << END
 [Unit]
 Description=SSH OHP Redirection Service
-Documentation=https://t.me/@Baung2012
+Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
 
 [Service]
@@ -46,7 +47,7 @@ END
 cat > /etc/systemd/system/dropbear-ohp.service << END
 [Unit]]
 Description=Dropbear OHP Redirection Service
-Documentation=https://t.me/@Baung2012
+Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
 
 [Service]
@@ -67,7 +68,7 @@ END
 cat > /etc/systemd/system/openvpn-ohp.service << END
 [Unit]]
 Description=OpenVPN OHP Redirection Service
-Documentation=https://t.me/@Baung2012
+Documentation=https://t.me/Baung2012
 After=network.target nss-lookup.target
 
 [Service]
