cad40ab1c31e603d369ae7763daef2e259e6aef0
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-07-07 13:48:55 +0800

    @Baung

--- menu/copy.sh
+++ menu/copy.sh
@@ -1,129 +1,173 @@
 #!/bin/bash
-# //====================================================
-# //	System Request:Debian 9+/Ubuntu 18.04+/20+
-# //	Author:	Tarap-Kuhing
-# //	Dscription: Xray Menu Management
-# //	email: merahjambo@gmail.com
-# //  telegram: https://t.me/Baung2012
-# //====================================================
-# // font color configuration | TARAP KUHING AUTOSCRIPT
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/kuhing/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m"
+export COLOR1="$(cat /etc/kuhing/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/kuhing/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\033[0;33m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
 clear
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-CHATID=`cat /etc/per/id`
-KEY=`cat /etc/per/token`
-URL="https://api.telegram.org/bot$KEY/sendMessage"
-IP=$(curl -s ipv4.icanhazip.com);
-HOST="$(cat /etc/xray/domain)"
-DATEVPS=$(date +"%Y-%m-%d")
-LINKBACKUP="http://$HOST:81"
-ISPVPS=$(cat /etc/lokasi/isp)
-CITY=$(cat /etc/lokasi/city)
-WKT=$(curl -s ipinfo.io/timezone)
-GREEN="\e[92;1m"
-BLUE="\033[36m"
+# Color
 RED='\033[0;31m'
 NC='\033[0m'
-
-function BACKUPVPS() {
-mkdir -p /var/www/html/Tarap-Kuhing >/dev/null 2>&1
-cp -r /etc/xray/config.json  config.json >/dev/null 2>&1
-cp -r /etc/xray/ssh  ssh >/dev/null 2>&1
-zip config.json vps-backup.zip >/dev/null 2>&1
-zip ssh vps-backup.zip >/dev/null 2>&1
-mv *.zip /var/www/html/Tarap-Kuhing >/dev/null 2>&1
-systemctl restart backup >/dev/null 2>&1
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+TIMES="10"
+CHATID=$(cat /etc/per/id)
+KEY=$(cat /etc/per/token)
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+IP=$(wget -qO- ipinfo.io/ip);
+domain=$(cat /etc/xray/domain)
+date=$(date +"%Y-%m-%d")
+email=$(cat /root/email)
+if [[ "$email" = "" ]]; then
+echo -e "${YELLOW} Masukkan Email Untuk Menerima Backup ${NC}"
+read -rp "Email : " -e email
+cat <<EOF>>/root/email
+$email
+EOF
+fi
 clear
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "SUCCESSFULL BACKUP YOUR VPS" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "Please Save The Following Data" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}Your VPS IP${NC} : ${GREEN}$IP${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}DOMAIN${NC}      : ${GREEN}$HOST${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}DATE${NC}        : ${GREEN}$DATEVPS${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}ISP${NC}         : ${GREEN}$ISPVPS${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}LOCATION${NC}    : ${GREEN}$CITY${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${BLUE}LINK BACKUP${NC} : ${GREEN}$LINKBACKUP${NC}" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/Tarap-Kuhing/user.log
-echo -e "${RED}Please Save your backup file${NC}" | tee -a /etc/Tarap-Kuhing/user.log
+echo -e "\033[0;33m Mohon Menunggu , Proses Backup sedang berlangsung !!! \033[0m"
+rm -rf /root/backup
+mkdir /root/backup
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+echo -e ""
+echo -e "\033[0;33m Proses Backup Xray \033[0m"
+cp -r /etc/xray/config.json backup/xray
+sleep 2
+cp -r /etc/xray/ssh backup/xray
+sleep w
+cp -r /etc/per backup/per
+sleep w
+echo -e ""
+#echo -e "\033[0;33m Proses Backup Slowdns \033[0m"
+#cp -r /etc/slowdns backup/slowdns
+cd /root
+zip -r $IP-$date.zip backup > /dev/null 2>&1
+rclone copy /root/$IP-$date.zip dr:backup/
+url=$(rclone link dr:backup/$IP-$date.zip)
+id=(`echo $url | grep '^https' | cut -d'=' -f2`)
+link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 
-}
-
-
-function RESTOREVPS() {
-mv /var/www/html/Tarap-Kuhing/*.zip vps-backup.zip >/dev/null 2>&1
-rm -f *.zip >/dev/null 2>&1
-mv *.json /etc/xray >/dev/null 2>&1
-mv *.ssh /etc/xray >/dev/null 2>&1
-systemctl restart nginx >/dev/null 2>&1
-systemctl restart xray >/dev/null 2>&1
-systemctl restart backup >/dev/null 2>&1
+echo -e "
+====================≈===============
+Detail Backup TARAP KUHING TUNNELING
+====================================
+IP VPS        : $IP
+Link ID Backup: $id
+Tanggal       : $date
+====================================
+" | mail -s "Backup Data By TARAP KUHING TUNNELING" $email
+rm -rf /root/backup
+rm -r /root/$IP-$date.zip
 clear
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "SUCCESSFULL RESTORE YOUR VPS"
-echo -e "Please Save The Following Data"
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${BLUE}Your VPS IP${NC} : ${GREEN}$IP${NC}"
-echo -e "${BLUE}DOMAIN${NC}      : ${GREEN}$HOST${NC}"
-echo -e "${BLUE}DATE${NC}        : ${GREEN}$DATEVPS${NC}"
-echo -e "${BLUE}ISP${NC}         : ${GREEN}$ISPVPS${NC}"
-echo -e "${BLUE}LOCATION${NC}    : ${GREEN}$CITY${NC}"
-echo -e "${BLUE}LINK RESTORE${NC}: ${GREEN}$LINKBACKUP${NC}"
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${RED}This feature can only be used if the previous data backup uses this script${NC}"
-
-}
-
-function QUESTION() {
-clear
-echo -e ""
-echo -e "${RED}WARNING !!!${NC}"
-echo -e ""
-echo -e "${RED}If you haven't uploaded your file, the VPN will not connect${NC}"
-echo -e "${BLUE}if you have uploaded the file, please type yes${NC}"
-echo -e "${BLUE}if you haven't uploaded the file, please type not${NC}"
-read -p "Have you uploaded the backup file? yes/not :" NUM_MENU
- case $NUM_MENU in
-    yes)
-        RESTOREVPS
-    ;;
-    not)
-        menu
-    ;;
-    *)
-        echo -e "${Red}You wrong command !${Font}"
-        sleep 5
-        menu
-    ;;
-esac
-
-}
-
-
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[42m              BACKUP & RESTORE           \E[0m"
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-echo -e " [${grenbo}1${NC}] ${RED}• ${NC} Backup Data VPS"
-echo -e " [${grenbo}2${NC}] ${RED}• ${NC} Restore Data VPS"
-echo -e ""
-echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p "Select From Options [ 1 - 2 ] : " OPT_MENU
-echo -e ""
- case $OPT_MENU in
-    1)
-        BACKUPVPS
-    ;;
-    2)
-        QUESTION
-    ;;
-    *)
-        echo -e "${Red}You wrong command !${Font}"
-        sleep 5
-        menu
-    ;;
-esac
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+clear
+echo -e "
+==================================
+         Detail Backup 
+               By
+ TARAP KUHING TUNNELING { T.K.T }
+==================================
+IP VPS        : $IP
+Link ID Backup: $id
+Tanggal       : $date
+==================================
+"
 
+echo -e "\033[0;33m Cek Email Kamu Link ID Backup Sudah Dikirim \033[0m"
+echo -e "\033[0;33m               Atau  ${NC}"
+echo -e "\033[0;33m Copy Link ID Di Atas Dan Restore Di VPS Baru \033[0m"
+echo ""
+message="
+<code>===========================</code>
+<code>      Detail Backup    </code>
+<code>==========================</code>
+<code>  TARAP KUHING TUNNELING </code>
+<code>===========================</code>
+<code>DOMAIN        : ${domain}</code>
+<code>=========================/=</code>
+<code>IP VPS        : ${IP}</code>
+<code>===========================</code>
+<code>Link ID Backup:</code> $id
+<code>===========================</code>
+<code>Tanggal       : $date</code>
+<code>===========================</code>
+"
+curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+read -n 1 -s -r -p "Press any key to back on menu"
 menu
