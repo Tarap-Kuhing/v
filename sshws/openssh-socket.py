e705976d7ef4bca4bbea5053ebf8038c90ef0970
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-09-04 17:31:50 +0800

    @Baung

--- sshws/openssh-socket.py
+++ sshws/openssh-socket.py
@@ -1,18 +1,21 @@
-#!/usr/bin/python
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
-
-# Pass
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 2082 
+#Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
+#port opensssh
 DEFAULT_HOST = '127.0.0.1:22'
-RESPONSE = 'HTTP/1.1 101 <font color="green"><b><i>Bismillah Konek</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="green"><b>TARAP KUHING TUNNELING</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+#RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
     def __init__(self, host, port):
